a60060ccf04768b4e39bf552e0c2b44410f7c64f
sid-thakur47 <sidthakur258@gmail.com>
2020-06-18 17:06:52 +0530

    REFACTORED:Changes comments

--- src/test/java/com/bl/spotify/SpotifyTest.java
+++ src/test/java/com/bl/spotify/SpotifyTest.java
@@ -12,6 +12,9 @@ public class SpotifyTest {
     @param uid:ID of user
     @param TOKEN: Bearer token
     @param JSON:Request Header
+    @param plaList:to store multiple playlist
+    @param trackId:to store multiple track id
+    @param totalPlayList:total number of play list
      */
 
     public static String token = "";
